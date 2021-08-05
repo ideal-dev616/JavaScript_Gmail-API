e206c1687e51895e13cc7506c9d963187ac04e78
goodidea0616 <lixiangde888@gmail.com>
2021-08-05 20:32:31 +0900

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '01a403dde31467901ba79ff2db79c6d740733a5e',
+    'name' => '116/login',
+  ),
+  'versions' => 
+  array (
+    '116/login' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '01a403dde31467901ba79ff2db79c6d740733a5e',
+    ),
+  ),
+);
